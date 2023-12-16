068e2a7100f4fc0256904b05018b5056a5e9ebd4
Michael D. Sorochan Armstrong <45431766+mdarmstr@users.noreply.github.com>
2023-12-16 16:40:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ def main():
     description="tolerate module in Python",
     author="Mykhaylo",
     author_email="mdarmstr@ualberta.ca",
+    install_requires=['numpy'],
     ext_modules=[Extension("tolerate", ["tolerate.c"], 
                  include_dirs=[numpy.get_include()])],
   )
